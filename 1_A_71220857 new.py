5dc9444ef07a7734ef83b2e3ff29a61b8ce99e51
dailyariella <118499517+dailyariella@users.noreply.github.com>
2022-11-18 10:33:01 +0700

    Add files via upload

---
+++ 1_A_71220857 new.py
@@ -0,0 +1,5 @@
+#Test 1
+Nama=input(str("Masukkan nama anda :"))
+Matkul=input(str("Masukkan nama Matakuliah :"))
+Grup=input(str("Masukkan grup anda :"))
+print(f"Hallo ! {Nama} {Matkul} \n Anda tergolong dalam kelas {Matkul} pada grup {Grup}")
---
+++ 2_A_71220857.py
@@ -0,0 +1,4 @@
+#Test 2
+suhuF=int(input("Suhu dalam skala Fahrenheit :"))
+FkeC=int((5/9)*(suhuF-32))
+print (suhuF,"derajat Fahrenheit dikonversi menjadi", FkeC, "derajat Celcius")
